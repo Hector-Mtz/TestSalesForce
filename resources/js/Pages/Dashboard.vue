d9d49c05301f3adf178b344bfd30ddc1ba5432cc
Hector <hugo201123@gmail.com>
2024-07-11 17:10:24 -0600

    check new things

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,21 +1,42 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Welcome from '@/Components/Welcome.vue';
+//graficas 
+import Grafica1 from './Partials/Grafica1.vue'
 </script>
 
 <template>
     <AppLayout title="Dashboard">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
                 Dashboard
             </h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+        <div class="flex flex-row p-8 ">
+            <div class="grid w-3/4 grid-cols-3 col-start-1 col-end-3">
+                <!--Primera fila-->
+                <div class="col-start-1 col-end-2 row-start-1 row-end-2">
+                    <h1 class="text-2xl font-bold">Reportes</h1>
+                    <div class="bg-[#091a3e] rounded-lg p-2">
+                        <h1 class="font-bold text-white">Prospectos totales</h1>
+                        <h2 class="my-4 text-5xl font-bold text-center text-white">0</h2>
+                    </div>
                 </div>
+                <div class="col-start-2 col-end-3 row-start-1 row-end-2">
+
+                </div>
+                <div class="col-start-3 col-end-4 row-start-1 row-end-2">
+                    
+                </div>
+            </div>
+            <div class="w-1/4">
+               
+            </div>
+        </div>
+        <div class="flex flex-row p-8">
+            <div class="w-1/4 ">
+                <Grafica1 />
             </div>
         </div>
     </AppLayout>
---
+++ resources/js/Pages/Partials/Grafica1.vue
@@ -0,0 +1,145 @@
+<script setup>
+import * as am5 from '@amcharts/amcharts5';
+import * as am5xy from '@amcharts/amcharts5/xy';
+import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
+import { ref, onMounted } from 'vue'
+
+onMounted(() => 
+{
+am5.ready(function() {
+
+
+// Create root element
+// https://www.amcharts.com/docs/v5/getting-started/#Root_element
+var root = am5.Root.new("chartdiv");
+
+
+// Set themes
+// https://www.amcharts.com/docs/v5/concepts/themes/
+root.setThemes([
+  am5themes_Animated.new(root)
+]);
+
+
+// Create chart
+// https://www.amcharts.com/docs/v5/charts/xy-chart/
+var chart = root.container.children.push(am5xy.XYChart.new(root, {
+  panX: false,
+  panY: false,
+  wheelX: "panX",
+  wheelY: "zoomX",
+  paddingLeft: 0,
+  layout: root.verticalLayout
+}));
+
+// Add scrollbar
+// https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
+chart.set("scrollbarX", am5.Scrollbar.new(root, {
+  orientation: "horizontal"
+}));
+
+var data = [{
+  "year": "12",
+  "nuevo": 2.5,
+  "junk": 2.5,
+  "contactado": 2.1,
+}]
+
+
+// Create axes
+// https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+var xRenderer = am5xy.AxisRendererX.new(root, {
+  minorGridEnabled: true
+});
+var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+  categoryField: "year",
+  renderer: xRenderer,
+  tooltip: am5.Tooltip.new(root, {})
+}));
+
+xRenderer.grid.template.setAll({
+  location: 1
+})
+
+xAxis.data.setAll(data);
+
+var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+  min: 0,
+  max: 100,
+  numberFormat: "#'%'",
+  strictMinMax: true,
+  calculateTotals: true,
+  renderer: am5xy.AxisRendererY.new(root, {
+    strokeOpacity: 0.1
+  })
+}));
+
+
+// Add legend
+// https://www.amcharts.com/docs/v5/charts/xy-chart/legend-xy-series/
+var legend = chart.children.push(am5.Legend.new(root, {
+  centerX: am5.p50,
+  x: am5.p50
+}));
+
+
+// Add series
+// https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+function makeSeries(name, fieldName) {
+  var series = chart.series.push(am5xy.ColumnSeries.new(root, {
+    name: name,
+    stacked: true,
+    xAxis: xAxis,
+    yAxis: yAxis,
+    valueYField: fieldName,
+    valueYShow: "valueYTotalPercent",
+    categoryXField: "year"
+  }));
+
+  series.columns.template.setAll({
+    tooltipText: "{name}, {categoryX}:{valueYTotalPercent.formatNumber('#.#')}%",
+    tooltipY: am5.percent(10)
+  });
+  series.data.setAll(data);
+
+  // Make stuff animate on load
+  // https://www.amcharts.com/docs/v5/concepts/animations/
+  series.appear();
+
+  series.bullets.push(function () {
+    return am5.Bullet.new(root, {
+      sprite: am5.Label.new(root, {
+        text: "{valueYTotalPercent.formatNumber('#.#')}%",
+        fill: root.interfaceColors.get("alternativeText"),
+        centerY: am5.p50,
+        centerX: am5.p50,
+        populateText: true
+      })
+    });
+  });
+
+  legend.data.push(series);
+}
+
+makeSeries("Nuevo", "nuevo");
+makeSeries("Junk", "junk");
+makeSeries("Contactado", "contactado");
+
+
+
+// Make stuff animate on load
+// https://www.amcharts.com/docs/v5/concepts/animations/
+chart.appear(1000, 100);
+
+}); // end am5.ready()
+})
+</script>
+<template>
+    <div id="chartdiv"></div>
+</template>
+<style scoped>
+#chartdiv {
+    width: 100%;
+    height: 500px;
+  }
+</style>
