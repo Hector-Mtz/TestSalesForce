c64ebe099a3ac35f627116a91ae84d375389dd13
Hector <hugo201123@gmail.com>
2024-07-18 13:25:36 -0600

    save ruletas sedes

--- app/Http/Controllers/RuletaGeneralController.php
+++ app/Http/Controllers/RuletaGeneralController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Asignacione;
 use App\Models\ProductoDeInteres;
 use App\Models\RuletaGeneral;
+use App\Models\RuletaSede;
+use App\Models\RuletaSedeProductos;
+use App\Models\RuletaSedeSedes;
 use App\Models\Sede;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -45,12 +48,45 @@ class RuletaGeneralController extends Controller
         switch ($request['tipo_ruleta']) 
         {
             case 'ruleta_sede':
+                //primero creamos la ruleta para despues asociale las sedes y productos
+                $new_ruleta_sede = RuletaSede::updateOrCreate(
+                    ['nombre' => $request['nombre']],
+                    ['creado_por' => $request['creador'],
+                     'tipo_asignacion' => $request['asignacion'],
+                     'asignaciones' => 0
+                    ]);
+                
+                for ($i=0; $i < count($request['sedes']); $i++) 
+                { 
+                    $sede = $request['sedes'][$i];
+                    if($sede['inluir'] == true)
+                    {
+                       RuletaSedeSedes::updateOrCreate([
+                         'ruleta_sede_id' => $new_ruleta_sede['id'],
+                         'sede_id' => $sede['id']
+                       ]);
+                    }
+                }
+
+                for ($i=0; $i < count($request['producto_interes']) ; $i++) 
+                { 
+                   $producto = $request['producto_interes'][$i];
+                   if($producto['incluir'] == true)
+                   {
+                     RuletaSedeProductos::updateOrCreate([
+                        'ruleta_sede_id' => $new_ruleta_sede['id'],
+                        'producto_id' => $producto['id']
+                     ]);
+                   }
+                }
                 
                 break;
             case 'ruleta_padre':
                 break;
             case 'ruleta_asesores':
             break;
+
+            redirect()->back();
         }
     }
 
